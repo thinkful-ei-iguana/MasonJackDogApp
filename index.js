242ca46a6b91916162551c12cb045429f33d2341
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-01 12:24:15 -0400

    modded js

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+
+function getDogImage(number) {
+  if (typeof number === Number) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+  } else {
+    fetch('https://dog.ceo/api/breeds/image/random/3')
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+  }
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
+  responseJson.forEach()
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  );
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -21,7 +30,9 @@ function displayResults(responseJson) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let dogNumber = Number($(event.currentTarget).find('#dogNumber').val());
+    console.log(dogNumber);
+    getDogImage(dogNumber);
   });
 }
 
